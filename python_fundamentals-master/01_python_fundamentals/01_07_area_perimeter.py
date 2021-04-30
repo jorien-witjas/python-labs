d6701157ffe4d53e3de0bed5cb4f093eb71432b8
JH Witjas <jorienwitjas@MacBook-Pro-van-JH.local>
2021-04-30 18:03:57 +0200

    new labs for submission

--- python_fundamentals-master/01_python_fundamentals/01_07_area_perimeter.py
+++ python_fundamentals-master/01_python_fundamentals/01_07_area_perimeter.py
@@ -3,3 +3,12 @@
 Write the necessary code to display the area and perimeter of a rectangle that has a width of 2.4 and a height of 6.4.
 
 '''
+
+print(" --------")
+print("|        |")
+print("|        |")
+print("|        |")
+print("|        |")
+print("|        |")
+print("|        |")
+print(" --------")
