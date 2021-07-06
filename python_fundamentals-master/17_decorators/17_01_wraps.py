2f2e14facda0873b8e4370796a78dbd05dab5fab
JH Witjas <jorien.witjas@hotmail.com>
2021-07-06 11:22:27 +0200

    labs for 18_lamdas completed. Decorators not yet - will be completed after meeting w/ Gilad

--- python_fundamentals-master/17_decorators/17_01_wraps.py
+++ python_fundamentals-master/17_decorators/17_01_wraps.py
@@ -1,3 +1,28 @@
 """
 Write a decorator function that wraps text passed to it in a html <p> tag.
 """
+# def say_hi():
+#     print("Hi.")
+#
+# def say_moo():
+#     print("Moooo")
+#
+# list_ = [say_hi(), say_moo()]   #you store it in a list, but call it at the same time
+#
+# def outer_func():
+#     msg = "Weeeeeekend!"
+#     def inner_func():
+#         print(msg)
+#     return inner_func
+# outer_func()
+
+
+def make_text():
+    def wrapper():
+        print("Today it is 06 July."
+              "The day that I learn to make a decorator."
+              "That is awesome")
+
+    return wrapper
+
+
--- python_fundamentals-master/17_decorators/17_02_tags.py
+++ python_fundamentals-master/17_decorators/17_02_tags.py
@@ -3,3 +3,4 @@ Improve the decorator from the previous exercise by allowing it to take
 any specified HTML tag as an input - making it more general.
 
 '''
+
